45cafc76709664c6dde7d5979686d45884c9de99
yunsu <yunsu@DESKTOP-K41ULB4>
2021-09-17 11:30:29 +0900

    reviewService dao 추가

--- src/net/kh/review/model/service/ReviewService.java
+++ src/net/kh/review/model/service/ReviewService.java
@@ -1,5 +1,11 @@
 package net.kh.review.model.service;
 
-public class ReviewService {
+import net.kh.review.model.dao.ReviewDAO;
 
+public class ReviewService {
+	private ReviewDAO rDAO;
+	
+	public ReviewService() {
+		rDAO = new ReviewDAO();
+	}
 }
